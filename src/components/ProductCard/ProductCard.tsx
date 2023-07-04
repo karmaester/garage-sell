759de67988b2d947dc8b0102c7b4a4e828efb13b
Khristian Rojas <khristianrdev@gmail.com>
2023-07-03 22:44:45 -0500

    Add rendering conditionals to product values

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,10 +4,10 @@ export const ProductCard = ({ product }: { product: any }) => {
 
     return (
         <Card
-            image={product.image.url}
-            header={product.name}
-            meta={product.price.formatted_with_symbol}
-            description={product.description.replace(/(<([^>]+)>)/ig, "")}
+            image={product.image?.url !== null ? product.image?.url : ''}
+            header={product.name ? product.name : ''}
+            meta={product.price ? product.price.formatted_with_symbol : ''}
+            description={product.description ? product.description.replace(/(<([^>]+)>)/ig, "") : ''}
         />
     );
 };
